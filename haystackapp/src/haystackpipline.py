82f473cc082f38ef3ab9d20b6f03de253e20a620
Nolan Strait <nlstrait@gmail.com>
2021-09-24 12:23:53 -0700

    Update haystackpipline.py

--- haystackapp/src/haystackpipline.py
+++ haystackapp/src/haystackpipline.py
@@ -3,7 +3,7 @@ from haystackapp.apps import hspipe
 def search(query):
     result = ''
     result += 'Query: ' + query + '\n'
-    result += 'Answer: I don\'t know jack shit!'
+    result += 'Answer: I don\'t know anything!'
     if 'test' in hspipe:
         result += '\nHaystack pipeline is accessible.'
     return result
